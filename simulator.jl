e0bd3089abfe67cc55421f0a46f1062884fa6269
mrdaly <30601477+mrdaly@users.noreply.github.com>
2021-11-12 23:44:53 -0500

    added to simulator

--- simulator.jl
+++ simulator.jl
@@ -1,1 +1,26 @@
 include("ColorCode.jl")
+
+function simulate(str,outFileName)
+  assignment = Dict([(k,rand(1:2)) for k in keys(keyboardStrings)])
+
+  prior = Dict{Symbol,Float64}([letter => 1.0/nChoices for letter in keys(keyboardStrings)])
+  belief = Belief(prior,9,1)
+  certaintyThreshold = 0.95
+
+  for c in str
+
+  end
+end
+
+
+if length(ARGS) != 2
+    error("usage: julia simulator.jl <infile>.txt <outfile>.out")
+end
+
+inputfilename = ARGS[1]
+outputfilename = ARGS[2]
+
+open(inputfilename) do f
+  str = chomp(uppercase(read(f,String)))
+  simulate(str,outputfilename)
+end
